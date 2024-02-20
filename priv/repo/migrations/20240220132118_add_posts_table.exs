def0ba4d5de12cd3162e00cd2805e65adae38f83
tadeifelipe <felipe_santostadei@hotmail.com>
2024-02-20 10:25:44 -0300

    create migrations

---
+++ priv/repo/migrations/20240220132118_add_posts_table.exs
@@ -0,0 +1,13 @@
+defmodule Twix.Repo.Migrations.AddPostsTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:posts) do
+      add :text, :text
+      add :likes, :integer, default: 0
+      add :user_id, references(:users, on_delete: :delete_all)
+    end
+
+    create index(:posts, [:user_id])
+  end
+end
